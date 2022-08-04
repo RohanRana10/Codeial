d7590e6c0dfebe07d4af5260346de3ca7cdb7861
Rohan Rana <rohanrana.mail@gmail.com>
2022-08-04 13:45:09 +0530

    added user profile and user posts

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,20 @@
 const express = require('express');
 const router = express.Router();
 
+//below are names that import controllers
 const homeController = require('../controllers/home_controller');
+
 
 console.log('router loaded');
 
+//url call pe
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+router.use('/users',require('./posts'));
+
+//for any further routes, access from here
+//router.use('/routerName',require('./routerFile'));
 
 //exported the router
 module.exports = router;
